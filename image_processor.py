c83f056df83de266bc7498f5e9554101a3c47731
Vito Royeca <vito.royeca@gmail.com>
2026-03-20 11:24:05 -0400

    reverted the image_processor

--- image_processor.py
+++ image_processor.py
@@ -113,41 +113,46 @@ class ImageProcessor:
         cv2.imwrite(self.output_path, cropped)
         self.image = cropped.copy()
 
-    def gray_image(self):
-        image_copy = self.image.copy()
-
-        grayed = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
-        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_04_grayed.jpg")
-        cv2.imwrite(self.output_path, grayed)
-        self.image = grayed.copy()
-
-    def blurr_image(self):
-        image_copy = self.image.copy()
-
-        ret, thresh = cv2.threshold(image_copy, 130, 255, 0) #130 is the threshold value, 0-255, I found it via experimentation
-        blurred = cv2.GaussianBlur(thresh, (5, 5), 0)
-        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_05_blurred.jpg")
-        cv2.imwrite(self.output_path, blurred)
-        self.image = blurred.copy()
-
-    def edge_image(self):
-        image_copy = self.image.copy()
-
-        edges = cv2.Canny(image_copy, 50, 150)
-        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_06_edges.jpg")
-        cv2.imwrite(self.output_path, edges)
-        self.image = edges.copy()
-
     def trim_image(self):
         image_copy = self.image.copy()
 
-        contours, _ = cv2.findContours(image_copy, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        #By now we have just the inside of our markers, it's time to find the card and essentially do the same thing
+
+        #We'll convert the image to grayscale and then use a threshold to make it black and white, and blur it
+        #   The idea being the card is colorful and will result in a black rectange surrounding our card
+        #   The blur helps smooth out the image, and account for noise
+        grayed = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_04_grayed.jpg")
+        cv2.imwrite(self.output_path, grayed)
+        
+
+        ret, thresh = cv2.threshold(grayed, 130, 255, 0) #130 is the threshold value, 0-255, I found it via experimentation
+        blurred = cv2.GaussianBlur(thresh, (5, 5), 0)
+        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_05_blurred.jpg")
+        cv2.imwrite(self.output_path, blurred)
+
+        #OpenCV can find the edges of what is hopefully our card
+        edges = cv2.Canny(blurred, 50, 150)
+        self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_06_edges.jpg")
+        cv2.imwrite(self.output_path, edges)
+
+        #Find contours of the edges, the idea being the contour will "outline" the card for us
+        #   What's a contour? Basically a list of points that make up a shape
+        contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        # The hope being the outline of the card should be the largest contour/shape
+        #   There will be smaller contours/shapes, in the art and what not, but they should be smaller than the card!
         largest_contour = max(contours, key=cv2.contourArea)
+
         epsilon = 0.02 * cv2.arcLength(largest_contour, True)
         approx = cv2.approxPolyDP(largest_contour, epsilon, True)
 
+        # We might have something that isn't a perfect rectangle
+        # So we'll try to pick points along our shape and pick the 4 that make the largest area
+        # This should be the 4 corners of the card
         max_area = 0
         best_quad = None
+        # We'll try every combination of 4 points and pick the one with the largest area
         for quad in combinations(approx, 4):
             quad = np.array(quad, dtype="float32")
             area = cv2.contourArea(quad)
@@ -156,17 +161,26 @@ class ImageProcessor:
                 best_quad = quad
 
         if best_quad is None:
+            print("No best_quad found.")
             self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_01_orig.jpg")
             return
 
-        points = np.squeeze(best_quad)
+        points = np.squeeze(best_quad) #Best quad is a list of a list of points, we just want a list of points
+
+        #Sort the points so their in clockwise order ie
+        # . Origin
+        #
+        #   0 1
+        #   3 2
+        # 
+        # v+Y   >+X
         rect = np.zeros((4, 2), dtype="float32")
-        s = points.sum(axis=1)
-        rect[0] = points[np.argmin(s)] 
-        rect[2] = points[np.argmax(s)]
-        diff = np.diff(points, axis=1)
-        rect[1] = points[np.argmin(diff)]
-        rect[3] = points[np.argmax(diff)]
+        s = points.sum(axis=1) #Add together each points X and Y
+        rect[0] = points[np.argmin(s)] #Top left will have the smallest X, and smallest Y, and as such the smallest sum
+        rect[2] = points[np.argmax(s)] #Bottom right will have the largest X, and largest Y, and as such the largest sum
+        diff = np.diff(points, axis=1) #Subtract the X from the Y
+        rect[1] = points[np.argmin(diff)] #Top right will have the largest X and largest Y, so the smallest difference
+        rect[3] = points[np.argmax(diff)] #Bottom left will have the smallest X and smallest Y, so the largest difference
 
         top_width =  rect[1][0] - rect[0][0] 
         bottom_width = rect[3][0] - rect[2][0]
@@ -177,6 +191,7 @@ class ImageProcessor:
         width = int(max(top_width, bottom_width))
         height = int(max(left_height, right_height))
 
+        # # Define the destination points
         dst = np.array([
             [0, 0],
             [width - 1, 0],
@@ -184,9 +199,10 @@ class ImageProcessor:
             [0, height - 1]
         ], dtype="float32")
 
+        # Perform the perspective transformation
         matrix = cv2.getPerspectiveTransform(rect, dst)
-
         trimmed = cv2.warpPerspective(image_copy, matrix, (width, height))
+
         self.output_path = os.path.join(tempfile.gettempdir(), os.path.basename(self.image_path) + "_07_trimmed.jpg")
         cv2.imwrite(self.output_path, trimmed)
         self.image = trimmed.copy()
@@ -196,9 +212,6 @@ class ImageProcessor:
 
         if box_corners is not None and len(box_corners) == 4:
             self.crop_image(box_corners)
-            self.gray_image()
-            self.blurr_image()
-            self.edge_image()
             self.trim_image()
         return self.output_path
 
