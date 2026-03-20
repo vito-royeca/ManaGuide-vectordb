1bc965b62d7c2a5093974ea24095b76447f6d416
Vito Royeca <vito.royeca@gmail.com>
2026-03-19 22:41:46 -0400

    bulk update and insert

--- generate.py
+++ generate.py
@@ -1,10 +1,14 @@
 import os
 import sys
+import time
 from datetime import datetime
+from timeit import default_timer as timer
+from datetime import timedelta
 
 import psycopg
 from pgvector.psycopg import register_vector
 from PIL import Image
+from psycopg import sql
 import torch
 from torchvision import models, transforms
 from torchvision.models import ResNet18_Weights
@@ -78,23 +82,106 @@ def create_cmimage(path, card_id):
             print(f"Already exists... {card_id}")
     cursor.close()
 
+def batch_process_cmimages(path_dictionary):
+    cursor = connection.cursor()
+    card_ids = path_dictionary.keys()
+    found_card_values = {}
+    missing_card_ids = []
+    batch_insert_values = []
+    batch_update_values = []
+    
+    query = sql.SQL("SELECT cmcard, date_updated FROM cmimage WHERE cmcard IN ({})").format(
+        sql.SQL(',').join(map(sql.Literal, card_ids)))
+    cursor.execute(query)
+    for record in cursor:
+        found_card_values[record[0]] = record[1].astimezone(tz=None)
+    missing_card_ids = [card_id for card_id in card_ids if card_id not in found_card_values.keys()]
+
+    # filter out unupdated cards, i.e compare file_timestamp vs. db_timestamp
+    for k, v in found_card_values.items():
+        file_name = path_dictionary[k]
+        file_timestamp = datetime.fromtimestamp(os.path.getmtime(file_name)).astimezone(tz=None)
+
+        if file_timestamp != v:
+            print(f"\tmismatch -  file: {file_timestamp} vs db: {v} for card {k}")
+            embeddings = create_embeddings(file_name)
+            batch_update_values.append((embeddings, file_timestamp, k))
+
+    # bulk update
+    if len(batch_update_values) > 0:
+        print(f"\tUpdating {len(batch_update_values)} rows...")
+        query = """
+            UPDATE cmimage i 
+            SET embeddings = j.embeddings, date_updated = j.date_updated 
+            FROM VALUES(%s) as j(cmcard, embeddings, date_updated)
+            WHERE i.cmcard = j.cmcard;
+        """
+        ([psycopg.ClientCursor(connection).mogrify(query, params) for params in batch_update_values])
+        cursor.executemany(query, params_seq=batch_update_values)
+        connection.commit()
+
+    # check if missing_card_ids are in cmcard table before inserting
+    if len(missing_card_ids) > 0:
+        query = sql.SQL("SELECT new_id FROM cmcard WHERE new_id IN ({})").format(
+            sql.SQL(', ').join(map(sql.Literal, missing_card_ids)))
+        cursor.execute(query)
+        for record in cursor:
+            file_name = path_dictionary[record[0]]
+            file_timestamp = datetime.fromtimestamp(os.path.getmtime(file_name)).astimezone(tz=None)
+            embeddings = create_embeddings(file_name)
+            batch_insert_values.append((record[0], embeddings, file_timestamp))
+
+    # bulk insert
+    if len(batch_insert_values) > 0:
+        print(f"\tInserting {len(batch_insert_values)} rows...")
+        query = """
+            INSERT INTO cmimage(cmcard, embeddings, date_updated) VALUES (%s, %s, %s)
+        """
+        ([psycopg.ClientCursor(connection).mogrify(query, params) for params in batch_insert_values])
+        cursor.executemany(query, params_seq=batch_insert_values)
+        connection.commit()
+
 def optimize_database():
     cursor = connection.cursor()
     cursor.execute("DROP INDEX IF EXISTS cmcard_index_embeddings;")
     cursor.execute("CREATE INDEX cmcard_index_embeddings ON cmimage USING ivfflat (embeddings vector_l2_ops) WITH (lists = 100);")
     connection.commit()
 
+def format_time(sec):
+   return timedelta(seconds=sec)
+   
 def process_images(image_path, card_id=None):
+    alpha = timer()
+
     if os.path.isdir(image_path):
+        batch_count = 1000
+        total_count = 0
+        path_dictionary = {}
+
         for root, dirs, files in os.walk(image_path):
             for file in files:
                 if file == "normal.jpg":
                     path = os.path.join(root, file)
                     card_id = path.split(os.path.sep)[-4] + "_" + path.split(os.path.sep)[-3] + "_" + path.split(os.path.sep)[-2]
-                    create_cmimage(path, card_id)
+                    path_dictionary[card_id] = path
 
+                if len(path_dictionary.keys()) >= batch_count:
+                    start = timer()
+                    total_count += batch_count
+                    batch_process_cmimages(path_dictionary)
+                    path_dictionary.clear()
+                    end = timer()
+                    print(f"Processed {total_count} rows in {format_time(end-start)}")
+
+        if len(path_dictionary.keys()) >= 1:
+            batch_process_cmimages(path_dictionary)
+        path_dictionary.clear()
+        print(f"Total rows: {total_count}")
     else:
         create_cmimage(image_path, card_id)
+
+    omega = timer()
+    print(f"Total time: {format_time(omega-alpha)}")
 
 def main(image_path, card_id=None):
     init_model()
